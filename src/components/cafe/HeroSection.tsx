b9bef25a205a6bb6a6eb813dc7638a9726f283dd
Biriy <biriyasunil25@gmail.com>
2026-01-16 11:00:26 +0530

    Initial commit: cozy cafe demo website

--- src/components/cafe/HeroSection.tsx
+++ src/components/cafe/HeroSection.tsx
@@ -65,7 +65,7 @@ const HeroSection = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.2 }}
-          className="absolute bottom-8 left-1/2 -translate-x-1/2"
+          className="absolute -bottom-36 left-1/2 -translate-x-1/2"
         >
           <a
             href="#why-us"
